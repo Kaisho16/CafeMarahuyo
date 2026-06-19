c5a940fb94f69bacb0d6fcb3179912d511368bfc
Kaisho16 <94833627+Kaisho16@users.noreply.github.com>
2026-06-19 22:45:34 +0800

    FIX PROMO

---
+++ src/CafeMarahuyo.Shared/Migrations/PosDb/20260619144323_AddValidUntilToPromo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeMarahuyo.Shared.Migrations.PosDb
+{
+    /// <inheritdoc />
+    public partial class AddValidUntilToPromo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
